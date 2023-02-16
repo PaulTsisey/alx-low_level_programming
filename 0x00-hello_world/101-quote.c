c6c2df4e47b9e81bb518764fbf220b8744d704ab
PaulTsisey <tpaulkwasah@gmail.com>
2023-02-16 13:57:50 -0800

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  *main - prints and that piece of art is useful" -Dora Korpar, 2015-10-19
  *followed by a new line, to the standard error
- *Return: 1
+ *Return: Always 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 } 
