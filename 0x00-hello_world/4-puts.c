0c6f96c6809857a9004fa3a14e64fa1e99f81e8e
PaulTsisey <tpaulkwasah@gmail.com>
2023-02-16 10:51:27 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main -  using the function puts to print.
  * followed by new line.
- * return 0.
+ * Return: always 0
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 
 }
