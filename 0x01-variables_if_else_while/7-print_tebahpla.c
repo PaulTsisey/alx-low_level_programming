87a59b494dbdb19ce1c1996ff850c0d5d1f97951
PaulTsisey <tpaulkwasah@gmail.com>
2023-02-18 05:12:42 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints lowercase alphabets in reverse
+ *followed by a new line
+ *Return:always 0
+ */
+
+int main(void)
+{
+	char p;
+
+	p = 'z';
+	while
+		(p >= 'a')
+		{
+			putchar(p);
+			p--;
+		}
+	putchar('\n');
+	return (0);
+}
