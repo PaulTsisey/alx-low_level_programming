b2edecaf49516fd60026c7ec454b45e94bbc1a42
PaulTsisey <tpaulkwasah@gmail.com>
2023-02-21 13:00:24 -0800

    Completed task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main()
 {
-	int putchar("_");
-	int putchar("p");
-	int putchar("u");
-	int putchar("t");
-	int putchar("c");
-	int putchar("h");
-	int putchar("a");
-	int putchar("r\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r\n");
 
 	return (0);
 }
