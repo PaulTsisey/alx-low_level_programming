502a6efbff982d92bbd3000c2015fa6f20ae6e55
PaulTsisey <tpaulkwasah@gmail.com>
2023-02-21 12:45:03 -0800

    Completed the first task"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * followed by a new line
+ * Return: Always 0
+ */
+
+int main()
+{
+	int putchar("_");
+	int putchar("p");
+	int putchar("u");
+	int putchar("t");
+	int putchar("c");
+	int putchar("h");
+	int putchar("a");
+	int putchar("r\n");
+
+	return (0);
+}
