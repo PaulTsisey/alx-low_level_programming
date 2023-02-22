06417590533a67486422d40d827c880e7f99c60c
PaulTsisey <tpaulkwash@gmail.com>
2023-02-22 09:19:29 -0800

    Task 8. completed

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every munite of the of Jack Bauer
+ * starting from 00:00 to 23:59.
+ *
+ * Return: void.
+ */
+
+void jack_bauer(void)
+{
+	int t, i, m, e;
+
+	for (t = 0; t <= 2; t++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if ((t <= 1 && i <= 9) || (t <= 2 && i <= 3))
+			{
+				for (m = 0; m <= 5; m++)
+				{
+					for (e = 0; e <= 9; e++)
+					{
+						_putchar(t + '0');
+					_putchar(i + '0');
+				_putchar(58);
+			_putchar(m + '0');
+		_putchar(e + '0');
+	_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
