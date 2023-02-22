cc2384f34f5c26b8a5284fa27751fb84681f46dd
PaulTsisey <tpaulkwash@gmail.com>
2023-02-22 12:41:17 -0800

    Task 10. completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i1: The first integer
+ * @i2: The second integer
+ * Return: The result.
+ */
+
+int add(int i1, int i2)
+{
+	return (i1 + i2);
+}
