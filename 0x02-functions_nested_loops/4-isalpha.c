dea1d14a732add3a4ea9a471e77490ce164bbb38
PaulTsisey <tpaulkwash@gmail.com>
2023-02-22 06:19:20 -0800

    Completed task 4.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,16 @@
 
 /**
  * _isalpha - Checkes for alphabetic character
- * @c: - The character to be checked
- * Returns: 1 for alphabetic character or 0 for anything else
+ * @c: The character to be checked
+ * Returns: 1 for _isalpha or 0 for anything else
  */
 
 int _isalpha(int c)
+
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
