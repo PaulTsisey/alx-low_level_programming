daa584cffdc0ad053420ea9695b21155fafa7356
PaulTsisey <tpaulkwash@gmail.com>
2023-02-24 15:03:19 -0800

    File 4-mai.c and 4-print_most_numbers.c for task 4 added.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
+ * do not print 2 and 4
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	while (c = 48; c <= 57; c++)
+	{
+		continue
+			(c = 50 || c = 52);
+		_putchar(c);
+	}
+	_putchar('\n');
+}
