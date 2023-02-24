b6658ce4894921de0de8ac13422270f34c56d5a0
PaulTsisey <tpaulkwash@gmail.com>
2023-02-24 14:34:33 -0800

    Files 2-mul.c and 2-main.c added for task 2.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - checks for checks for a digit (0 through 9)
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: The answer.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
