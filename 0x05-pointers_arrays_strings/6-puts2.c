345e795776ee9011ff71990ef43c0ed88a53ec1a
PaulTsisey <tpaulkwash@gmail.com>
2023-02-28 15:06:43 -0800

    Task 6. complete

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/** 
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * followed by a new line
+ * @str: string
+ * Return: void
+ */
+
+void puts@(char *str)
+{
+	int n = 0, t;
+
+	while (*str)
+	{
+		n++;
+		str++;
+	}
+	for (t = 0; t < n; t++)
+		str--;
+	for (t = 0; t < n; t++)
+	{
+		if (t % 2 ==0)
+			_putchar(str[t]);
+	}
+	_putchar('\n');
+}
