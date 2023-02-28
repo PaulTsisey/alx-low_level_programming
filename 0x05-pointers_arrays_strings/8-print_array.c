59524810f49bff0f335f2971ce1c2f65a0bb9caa
PaulTsisey <tpaulkwash@gmail.com>
2023-02-28 15:45:40 -0800

    Task 8. complete

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,15 @@
 
 void print_array(int *a, int n)
 {
-	int m = 0;
-	
-	while (m < n)
+	int m;
+
+	for (m = 0; m < (n - 1); m++)
 	{
 		printf("%d", a[m]);
-		if (m != (n -1))
+		if (m == (n - 1))
+		{
+			printf("%d", a[n - 1]);
 			printf(", ");
-		m++;
 	}
 	_putchar('\n');
 }
