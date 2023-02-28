f7adc81ab29bb5b6d150f4842587def3f293dc14
PaulTsisey <tpaulkwash@gmail.com>
2023-02-28 15:37:42 -0800

    Task 8. complete

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: elements in array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int m = 0;
+	
+	while (m < n)
+	{
+		printf("%d", a[m]);
+		if (m != (n -1))
+			printf(", ");
+		m++;
+	}
+	_putchar('\n');
+}
