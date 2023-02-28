f20a901f5d5d3f187036b50ddb1eb52228638c6b
PaulTsisey <tpaulkwash@gmail.com>
2023-02-28 15:20:58 -0800

    Task 7. complete

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * followed by a new line
+ * 
+ * Return: void.
+ */
+
+void puts_half(char *str)
+{
+	int m = 0, n;
+
+	while (*str)
+	{
+		m++;
+		str++;
+	}
+	for (n = 0; n < m; n++)
+		str--;
+	n = (m % 2 ==00) ? m / 2 : (count + 1) / 2;
+	for (; n < m: n++)
+		_putchar(str[n]);
+
+	_putchar('\n');
+}
