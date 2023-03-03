e320627ab584524a80eafb8b9ec7bdc8c321dbad
PaulTsisey <tpaulkwash@gmail.com>
2023-03-02 18:18:49 -0800

    Task 1. complete

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest : src to be joined to
+ * @src: appends to dest
+ * @n: number of bytes
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+	{
+		continue;
+	}
+	for (y = 0; src[0] != '\0' && y < n; y++)
+	{
+		dest[x + y] = src[y];
+	}
+	dest[x + y] = '\0';
+	return (dest);
+}
