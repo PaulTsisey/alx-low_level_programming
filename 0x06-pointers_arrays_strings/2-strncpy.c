62e150ab976a8b4d941daeeb1e76fe96ea414554
PaulTsisey <tpaulkwash@gmail.com>
2023-03-02 19:12:52 -0800

    Task 2. complete

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a;
+	int a = 0, b = 0;
 
-	for (a = 0; a < n && *(src + a); a++)
-	{
-		*(dest + a) = *(src + a);
-	}
-	for (; a < n; a++)
-	{
-		*(dest + a) = '\0';
-	}
+	while (src[a++])
+		b++;
+	for (a = 0; src[a] && a < n; a++)
+		dest[a] = src[a];
+	for (a = b; a < n; a++)
+		dest[a] = '\0';
+
 	return (dest);
+}
