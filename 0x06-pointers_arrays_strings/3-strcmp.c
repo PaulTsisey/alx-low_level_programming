e1c4f8b3bd3d25cd9ea7a50b7b2c48a5c23941be
PaulTsisey <tpaulkwash@gmail.com>
2023-03-02 19:32:07 -0800

    Task 3. complete

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "mai.h"
+
+/**
+ * _strcomp - compares two strings
+ * @s1: string one to be compared 
+ * @s2; string two to be compared
+ * Return: value after subtracting the strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
